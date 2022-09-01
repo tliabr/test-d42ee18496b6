98410cb9f2f6b8e5d6caf043d58975b9ba40b704
Thallia Borba Rosa Salbego <thallia@patchwork.health>
2022-09-01 17:49:27 +0100

    added loop to group shifts by worker

--- app/services/compliance/shifts_checker.rb
+++ app/services/compliance/shifts_checker.rb
@@ -2,16 +2,28 @@
 
 module Compliance
   class ShiftsChecker
-    attr_reader :shifts
+    attr_reader :shifts, :workers
 
-    def initialize(shifts:)
+    def initialize(shifts:, workers:)
       @shifts = shifts
+      @workers = workers
     end
 
     def run
       failing_shifts = []
+
+      #To group every worker's shifts
+      workers_shifts = []
+
+      #Checking if wach individual shift is longer than 13 hours
       shifts.each do |shift|
         failing_shifts << shift if shift.ends_at - shift.starts_at > 13.hours
+      end
+
+      #grouping worker's shifts
+      workers.each do |worker|
+        workers_shifts << Shift.where(worker: worker)
+      #I now have to group by the day to check the starts_at and ends_at of each day of every worker
       end
 
       { max_hours: { fail: failing_shifts.any?, shifts: failing_shifts } }
